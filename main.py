a196c3a1d9f7cf1e40094fc974fe498d8397c2ca
Niels Feij <nielsfeij@hotmail.com>
2022-11-01 22:30:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import totoScraper
 import unibetScraper
 import merger
+import arbitrage_calculator
 
 if __name__ == "__main__":
     toto = totoScraper.main()
     unibet = unibetScraper.main()
     scrapes = [toto, unibet]
-    merger.merge(scrapes)
+    merged = merger.merge(scrapes)
+    arbitrage_calculator.calculate(merged)
